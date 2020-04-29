64c82dd58cda2541eceaf6376d716ce8f67348d2
Julian Wong <julianwong@ucsb.edu>
2020-04-28 17:23:26 -0700

    database progress

--- middleware/database.js
+++ middleware/database.js
@@ -12,7 +12,7 @@ const client = new MongoClient(
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
-  req.db = client.db("Plant");
+  req.db = client.db("MCT");
   return next();
 }
 
