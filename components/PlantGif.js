df542c61328ebb1dc30193b2d2908c06993daaab
Leron <lreznikov@ucsb.edu>
2020-05-16 15:13:40 -0700

    fixed null exception

--- components/PlantGif.js
+++ components/PlantGif.js
@@ -40,9 +40,10 @@ class PlantGif extends React.Component {
       this.plant = 0;
     }
     console.log(this.plant);
-    console.log(plantLevel.testList);
-
-    this.firstLevel = plantLevel.testList;
+    if (plantLevel) {
+      console.log(plantLevel.testList);
+      this.firstLevel = plantLevel.testList;
+    }
 
     this.setState({ update: 0 });
   }
